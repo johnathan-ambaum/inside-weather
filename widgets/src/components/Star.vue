ba873b25a4cbbd5b146b3b13880bdfa670bf4f0f
Tanner Kozlowski <tanner.kozlowski@gmail.com>
2019-05-20 16:40:23 -0400

    built star component

---
+++ widgets/src/components/Star.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="ReviewItem__Right--Star">
+    <font-awesome-icon
+      v-for="i in starCount"
+      :key="i"
+      :icon="['fal', 'star']"
+      :style="{ color: 'black' }"
+    />
+  </div>
+</template>
+
+<script>
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faStar, faArrowLeft } from '@fortawesome/pro-light-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faStar);
+
+export default {
+  components: {
+    FontAwesomeIcon,
+  },
+
+  props: {
+    starCount: { type: Number, default: 0 },
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../scss/variables';
+@import '../scss/mixins';
+
+.fa-star {
+  margin-right: 12px;
+}
+</style>
