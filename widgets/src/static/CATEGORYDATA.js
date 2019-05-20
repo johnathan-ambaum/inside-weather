4175e4a115ac1cce35076e63820f9774f04520b1
Tanner Kozlowski <tanner.kozlowski@gmail.com>
2019-05-20 16:44:28 -0400

    defined category item data

---
+++ widgets/src/static/CATEGORYDATA.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    image: 'https://cdn.shopify.com/s/files/1/2994/0144/files/page_nav_sofette-min.png',
+    text: 'SOFAS & SOFETTES',
+    hoverColor: '#EEC4AF',
+    key: 'sofa',
+  },
+  {
+    image: 'https://cdn.shopify.com/s/files/1/2994/0144/files/page_nav_armchair-min.png',
+    text: 'ARMCHAIRS',
+    hoverColor: '#F3EACA',
+    key: 'armchairs',
+  },
+  {
+    image: 'https://cdn.shopify.com/s/files/1/2994/0144/files/page_nav_dining-min.png',
+    text: 'DINING CHAIRS',
+    hoverColor: '#D4D0CA',
+    key: 'diningchairs',
+  },
+  {
+    image: 'https://cdn.shopify.com/s/files/1/2994/0144/files/page_nav_lounge-min.png',
+    text: 'LOUNGE CHAIRS',
+    hoverColor: '#C7D0C7',
+    key: 'loungechairs',
+  },
+  {
+    image: 'https://cdn.shopify.com/s/files/1/2994/0144/files/page_nav_tables-min.png',
+    text: 'TABLES',
+    hoverColor: '#9DA4A8',
+    key: 'tables',
+  },
+  {
+    image: 'https://cdn.shopify.com/s/files/1/2994/0144/files/page_nav_storage-min.png',
+    text: 'STORAGE',
+    hoverColor: '#CE9F8E',
+    key: 'storage',
+  },
+];
