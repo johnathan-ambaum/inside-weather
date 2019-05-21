31330b0eb25262ac361f0deb6ddd4be5e7807488
Tanner Kozlowski <tanner.kozlowski@gmail.com>
2019-05-21 06:04:06 -0400

    built checkbox component

---
+++ widgets/src/components/SelectedCheckbox.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="SelectedCheckbox">
+    <span />
+	</div>
+</template>
+
+<style lang="scss">
+@import '../scss/variables';
+@import '../scss/mixins';
+
+.SelectedCheckbox {
+  & span {
+    width: 22px;
+    height: 22px;
+    display: block;
+    background: #B1C8CD;
+    border-radius: 50%;
+
+    &::after {
+      content: '\2714';
+      padding: 0 5px;
+      color: white;
+    }
+  }
+}
+</style>
