f8053429494595c5cdfd614e984c1c7f3bb58e92
Tanner Kozlowski <tanner.kozlowski@gmail.com>
2019-06-03 13:18:59 -0400

    updated verified checkmark component

--- widgets/src/components/SelectedCheckbox.vue
+++ widgets/src/components/SelectedCheckbox.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="SelectedCheckbox">
-    <img src='https://cdn.shopify.com/s/files/1/2994/0144/files/checkmark.png?254334' />
-	</div>
+  <div class="SelectedCheckbox" />
 </template>
 
 <style lang="scss">
@@ -9,6 +7,16 @@
 @import '../scss/mixins';
 
 .SelectedCheckbox {
-  display: flex;
+  background: url('https://cdn.shopify.com/s/files/1/2994/0144/files/checkmark_3x_daff5553-7f9e-4098-a799-30fb6dfee695.png?291296');
+  background-position: 50% 50%;
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 22px;
+  height: 22px;
+
+  @include at-query($breakpoint-small) {
+    width: 18px;
+    height: 18px;
+  }
 }
 </style>
