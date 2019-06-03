ab50a19a88c57691784806edf4ae44636ddd8b98
Tanner Kozlowski <tanner.kozlowski@gmail.com>
2019-06-03 13:16:18 -0400

    updated category key

--- widgets/src/static/CATEGORYDATA.js
+++ widgets/src/static/CATEGORYDATA.js
@@ -1,6 +1,6 @@
 export default [
   {
-    image: 'https://cdn.shopify.com/s/files/1/2994/0144/files/page_nav_sofette-min.png',
+    image: 'https://cdn.shopify.com/s/files/1/2994/0144/files/page_nav_sofa-min_edc49ec0-ade1-46ba-b065-84aa697f09d9.png?286687',
     text: 'Sofas & Sofettes',
     hoverColor: '#EEC4AF',
     key: 'sofas',
@@ -15,24 +15,24 @@ export default [
     image: 'https://cdn.shopify.com/s/files/1/2994/0144/files/page_nav_dining-min.png',
     text: 'Dining Chairs',
     hoverColor: '#D4D0CA',
-    key: 'diningchairs',
+    key: 'dining-chairs',
   },
   {
     image: 'https://cdn.shopify.com/s/files/1/2994/0144/files/page_nav_lounge-min.png',
     text: 'Lounge Chairs',
     hoverColor: '#C7D0C7',
-    key: 'loungechairs',
+    key: 'lounge-chairs',
   },
   {
     image: 'https://cdn.shopify.com/s/files/1/2994/0144/files/page_nav_tables-min.png',
     text: 'Tables',
     hoverColor: '#9DA4A8',
-    key: 'tables',
+    key: 'dining-tables,side-tables,coffee-tables',
   },
   {
     image: 'https://cdn.shopify.com/s/files/1/2994/0144/files/page_nav_storage-min.png',
     text: 'Storage',
     hoverColor: '#CE9F8E',
-    key: 'storage',
+    key: 'credenzas,sideboards',
   },
 ];
