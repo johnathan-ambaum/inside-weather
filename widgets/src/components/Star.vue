6dda51a01a0ae25678b6c158769db12a0b56c0c0
Tanner Kozlowski <tanner.kozlowski@gmail.com>
2019-06-03 13:18:13 -0400

    updated star component

--- widgets/src/components/Star.vue
+++ widgets/src/components/Star.vue
@@ -4,7 +4,7 @@
       v-for="i in starCount"
       :key="i"
       class="ReviewItemStar__Image"
-      src="https://cdn.shopify.com/s/files/1/2994/0144/files/star.png?254334"
+      src="https://cdn.shopify.com/s/files/1/2994/0144/files/star_2x_157511bd-230e-435d-a13f-242fd235281b.png?290387"
     />
   </div>
 </template>
@@ -18,11 +18,21 @@ export default {
 </script>
 
 <style lang="scss">
+@import '../scss/variables';
+@import '../scss/mixins';
+
 .ReviewItemStar {
   display: flex;
 
   &__Image {
     margin-right: 12px;
+    width: 18px;
+    height: 18px;
+
+    @include at-query($breakpoint-small) {
+      width: 15px;
+      height: 15px;
+    }
   }
 }
 </style>
