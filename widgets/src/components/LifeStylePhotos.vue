8c7184cc0bc757b8c19c6dd60ea0f3ea142f153f
Jishnu Parakkat <jishnu@enroco.com>
2019-07-24 11:33:08 +0530

    Lifestyle photos section with popup

--- widgets/src/components/LifeStylePhotos.vue
+++ widgets/src/components/LifeStylePhotos.vue
@@ -3,26 +3,173 @@
     class="LifeStylePhotos"
     ref="LifeStylePhotos"
   >
-    <heading-with-description>
-      <h2>{{sectionHeading}}</h2>
-      <p>{{sectionDescription}}</p>
-    </heading-with-description> 
+    <div 
+      class="--custom-container"
+    >
+      <heading-with-description>
+        <h2>{{sectionHeading}}</h2>
+        <p>{{sectionDescription}}</p>
+      </heading-with-description> 
+      <div 
+        class="LifeStylePhotos--gridContainer grid--container"
+      >
+        <div 
+          v-for="(item, index) in lifeStyleContent"
+          :key="item.$id"
+          v-bind:class="['LifeStylePhotos--column column LifeStylePhotos--column-'+ (index + 1)]"
+        >
+          <figure>
+            <img 
+              :src="item.image"
+            >
+          </figure>
+          <div 
+            class="more-info"
+            v-on:click="showPopUp"
+          >
+            <i class="more-info-trigger-icons info-icon-plus">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <g id="Group_318" data-name="Group 318" transform="translate(-343 -3113)">
+                  <circle id="Ellipse_9" data-name="Ellipse 9" cx="12" cy="12" r="12" transform="translate(343 3113)" fill="#efedea"/>
+                  <g id="Group_57" data-name="Group 57" transform="translate(349 3119)">
+                    <line id="Line_7" data-name="Line 7" y2="12" transform="translate(6)" fill="none" stroke="#7b7974" stroke-width="0.5"/>
+                    <line id="Line_8" data-name="Line 8" y1="12" transform="translate(12 6) rotate(90)" fill="none" stroke="#7b7974" stroke-width="0.5"/>
+                  </g>
+                </g>
+              </svg>
+            </i>
+          </div>
+        </div>
+      </div>
+    </div>
+    <pop-up
+      :PopUpImageSource="popUpMainImage"
+      popUpId="LifeStylePopUp"
+      :popUpHeading="popUpHeading"
+      :PopUpContentList="PopUpContentList"
+    >
+    </pop-up>
   </div>
 </template>
 <script>
 import HeadingWithDescription from './HeadingWithDescription.vue'
+import PopUp from './PopUp.vue'
 export default {
   components: {
     HeadingWithDescription,
+    PopUp
   },
   props: {
     sectionHeading: String,
-    sectionDescription: String
-  } 
+    lifeStyleItems: Array,
+    sectionDescription: String,
+    popUpHeading: String,
+    popUpList: Array
+  } ,
+  data() {
+    return {
+      lifeStyleContent: this.lifeStyleItems,
+      PopUpContentList: this.popUpList,
+      popUpMainImage: ''
+    }
+  },
+  methods: {
+    showPopUp: function(e) {
+      const currentParent = e.target.parentElement;
+      const findTarget = currentParent.querySelector('figure img');
+      this.popUpMainImage = findTarget.src;
+      const detectPopUp = document.getElementById('LifeStylePopUp');
+      detectPopUp.classList.add('fade');
+      setTimeout(function() {
+        detectPopUp.classList.add('in');
+      }, 300);
+    }
+  }
+
 }
 </script>
 <style lang="scss">
+@import '../scss/mixins';
+@import '../scss/variables';
+.grid--container {
+  width: auto;
+  margin: 66px -8px 0;
+}
+.LifeStylePhotos {
+  .HeadingWithDescription {
+    max-width: 655px;
+    margin: 0 auto;
+    h2 {
+      margin: 0 0 33px;
+    }
+  }
+  .more-info {
+    cursor: pointer;
+    height: 29px;
+    position: absolute;
+    bottom: 32px;
+    right: 28px;
+    width: 29px;
+    z-index: 9;
+    i {
+      pointer-events: none;
+      @include block(100%);
+      svg {
+        @include block(100%);
+      }
+    }
+  }
+}
+.LifeStylePhotos--gridContainer {
+  .LifeStylePhotos--column {
+    position: relative;
+    figure {
+      padding: 0 8px 16px;
+      @include block(100%);
+      img {
+        object-fit: cover;
+        @include block(100%);
+      }
+    }
+    &.LifeStylePhotos--column-1 {
+      grid-column-start: 1;
+      grid-column-end: span 13;
+      grid-row: 1 / auto;
 
+    }
+    &.LifeStylePhotos--column-2 {
+      grid-column-start: 14;
+      grid-column-end: -1;
+      grid-row: 1 / auto;
+    }
+    &.LifeStylePhotos--column-3 {
+      grid-column-start: 1;
+      grid-column-end: span 27;
+      grid-row: 2/span 2;
+    }
+    &.LifeStylePhotos--column-4, &.LifeStylePhotos--column-5{
+      grid-column-start: 28;
+      grid-column-end: -1;
+    }
+    &.LifeStylePhotos--column-6 {
+      grid-column-start: 1;
+      grid-column-end: span 27;
+    }
+    &.LifeStylePhotos--column-7 {
+      grid-column-start: 28;
+      grid-column-end: -1;
+    }
+    &.LifeStylePhotos--column-8, &.LifeStylePhotos--column-9{
+      grid-column-start: 1;
+      grid-column-end: span 13;
+    }
+    &.LifeStylePhotos--column-10 {
+      grid-column-start: 14;
+      grid-column-end: -1;
+      grid-row: 5/span 2;
+    }
+  }
+}
 </style>
 
 
---
+++ widgets/src/components/PopUp.vue
@@ -0,0 +1,209 @@
+<template>
+  <div
+    class="PopUp"
+    ref="PopUp"
+    :id="popUpId"
+  >
+    <div class="PopUp--dialog">
+      <div class="PopUp--body">
+        <div class="PopUp--imageWrapper">
+          <figure>
+            <img :src="PopUpImageSource">
+          </figure>
+        </div>
+        <div class="PopUp--content">
+          <button
+            class="PopUp--close"
+            @click="closePopUp(popUpId)"
+          >
+            <i>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17.414 17.414">
+                <g id="Group_176" data-name="Group 176" transform="translate(-1267.793 -302.793)">
+                  <line id="Line_117" data-name="Line 117" x2="16" y2="16" transform="translate(1268.5 303.5)" fill="none" stroke="#202020" stroke-width="2"/>
+                  <line id="Line_118" data-name="Line 118" x2="16" y2="16" transform="translate(1284.5 303.5) rotate(90)" fill="none" stroke="#202020" stroke-width="2"/>
+                </g>
+              </svg>
+            </i>
+          </button>
+          <h2 class="PopUp--heading">{{popUpHeading}}</h2>
+          <ul>
+            <li
+              v-for="item in PopUpContentList"
+              :key="item.$id"
+            >
+              <figure>
+                <img :src="item.image">
+              </figure>
+              <div>
+                <p>{{item.title}}</p>
+                <span>{{item.price}}</span>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    PopUpContentList: Array,
+    PopUpImageSource: String,
+    popUpId: String,
+    popUpHeading: String
+  },
+  methods: {
+    closePopUp(id) {
+      const getTarget = document.getElementById(id);
+      getTarget.classList.remove('in');
+      setTimeout(function() {
+        getTarget.classList.remove('fade');
+      }, 300);
+    }
+  }
+}
+</script>
+<style lang="scss">
+@import '../scss/mixins';
+@import '../scss/variables';
+.PopUp {
+  background: rgba($color: #000000, $alpha: 0.5);
+  display: none;
+  height: 100%;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  z-index: 99999;
+  .PopUp--dialog {
+    display: block;
+    opacity: 0;
+    height: auto;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 50%;
+    transform: translateY(-60%);
+    transition: all 0.3s ease-in-out;
+    margin: 0 auto;
+    width: 1000px;
+    .PopUp--body {
+      display: flex;
+      background: #ffffff;
+      justify-content: space-between;
+      .PopUp--imageWrapper {
+        flex-basis: 65%;
+        background: #202020;
+        figure {
+          background: #202020;
+          height: 0;
+          position: relative;
+          padding: 0 0 100%;
+          overflow: hidden;
+          img {
+            position: absolute;
+            left: 0;
+            right: 0;
+            top: 0;
+            bottom: 0;
+            height: auto;
+            margin: auto;
+            max-height: 100%;
+            max-width: 100%;
+            width: auto;
+          }
+        }
+      }
+      .PopUp--content {
+        flex-basis: 35%;
+        max-height: 650px;
+        padding: 25px 0 25px 30px;
+        position: relative;
+
+        ul {
+          overflow: hidden;
+          overflow-y: auto;
+          padding-right: 40px;
+          @include block(calc(100% - 65px));
+
+          li {
+            align-items: center;
+            display: flex;
+            justify-content: space-between;
+            margin: 0 0 25px;
+            width: 100%;
+
+            figure {
+              box-shadow: inset 0.2px 0.25px 0.25px 0px #202020;
+              height: 67px;
+              padding: 1px;
+              width: 67px;
+
+              img {
+                @include block();
+              }
+            }
+            div {
+              width: calc(100% - 70px);
+              padding-left: 10px;
+              p {
+                display: block;
+                font-family: $font-stack-avalon;
+                font-weight: 500;
+                margin: 0 0 5px;
+                @include fonts(14px,#202020,1.41,0.04em);
+              }
+              span {
+                display: block;
+                font-family: $font-stack-avalon;
+                font-weight: 500;
+                @include fonts(14px,#202020,1.41,0.04em);
+              }
+            }
+          }
+        }
+      }
+    } 
+  }
+  &.fade {
+    display: block;
+    &.in {
+      .PopUp--dialog {
+        opacity: 1;
+        transform: translateY(-50%);
+      }
+    }
+  }
+  .PopUp--close {
+    box-shadow: none;
+    cursor: pointer;
+    height: auto;
+    outline: none;
+    padding: 15px;
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: auto;
+    i {
+      width: 18px;
+      height: 18px;
+      display: block;
+      svg {
+        @include block(100%);
+      }
+    }
+  }
+  .PopUp--heading {
+    display: block;
+    font-family: $font-stack-avalon;
+    font-weight: $demi;
+    margin: 0 0 30px;
+    @include fonts(28px,#202020,1.27,0.04em);
+  }
+}
+</style>
+
+
