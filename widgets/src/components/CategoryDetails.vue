faee45067269f48127dc16469fc06eb7a2542b0c
Javeriya Javeriya <javeriya@enroco.com>
2019-07-24 17:13:52 +0530

    Added responsiveness and mobile UI for the category details

--- widgets/src/components/CategoryDetails.vue
+++ widgets/src/components/CategoryDetails.vue
@@ -82,6 +82,102 @@ export default {
       }
     }
   }
+  @include at-query('max-width: 1280px') {
+    .image-content-box {
+      .HeadingWithDescription {
+        padding: 0 0 0 67px;
+      }
+      &:nth-child(even) {
+        .HeadingWithDescription {
+          padding: 0 67px 0 75px;
+        }
+      }
+    }
+  }
+  @include at-query('max-width: 1199px') {
+    .image-content-box {
+      .HeadingWithDescription {
+        h2 {
+          font-size: 32px;
+        }
+      }
+    }
+  }
+  @include at-query('max-width: 991px') {
+    .image-content-box {
+      .HeadingWithDescription {
+        padding: 0 0 0 30px;
+        h2 {
+          font-size: 30px;
+          margin: 0 0 22px;
+        }
+      }
+      &:nth-child(even) {
+        figure {
+          flex-basis: 50%;
+        }
+        .HeadingWithDescription {
+          flex-basis: 50%;
+          padding: 0 30px 0 45px;
+        }
+      }
+    }
+  }
+  @include at-query('max-width: 767px') {
+    .image-content-box {
+      flex-wrap: wrap;
+      figure {
+        padding-bottom: 65.5%;
+      }
+      figure,
+      .HeadingWithDescription {
+        flex-basis: 100%;
+      }
+      .HeadingWithDescription {
+        margin: 0 27.5px;
+        position: relative;
+        background: #fff;
+        top: -4rem;
+        padding: 27px 23px;
+        h2 {
+          font-size: 20px;
+          font-weight: 500;
+        }
+        p {
+          @include fonts(12px,#202020,1.2,0.04em);
+          padding: 0;
+        }
+      }
+      &:nth-child(even) {
+        figure {
+          img {
+            width: 100%;
+          }
+        }
+        figure,
+        .HeadingWithDescription {
+          flex-basis: 100%;  
+        }
+        .HeadingWithDescription {
+          padding: 27px 23px;
+        }
+      }
+    }
+  }
+  @include at-query('max-width: 640px') {
+    .image-content-box {
+      .HeadingWithDescription {
+        top: -3.5rem;
+      }
+    }
+  }
+  @include at-query('max-width: 640px') {
+    .image-content-box {
+      .HeadingWithDescription {
+        top: -2.4rem;
+      }
+    }
+  }
 }
 </style>
 
