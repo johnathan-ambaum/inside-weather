275fbfc80861bccb026d48eb55314f7996faee9a
Michael Gibbons <michael@btownweb.com>
2020-02-21 16:55:28 -0700

    Updated Styles on modal

--- widgets/src/components/CustomizerProductModal.vue
+++ widgets/src/components/CustomizerProductModal.vue
@@ -1,13 +1,14 @@
 
 <template>
   <div v-if="showModal">
-    <div id="modal" class="product-customizer__modal">
-      <div class="product-customizer__modal-content">
-        <h3>Looking Good</h3>
-        <p>Nice Design! You are the first person to create this combination. We'd love to feature your item in a photoshoot!</p>
-        <img :src="productImages[0].thumbnail">
-        <button class="btn btn--full add-to-cart">Count Me In</button>
-        <button @click="hide">Close</button>
+    <div id="modal" class="product-customizer-modal">
+      <div class="product-customizer-modal__inner">
+        <close-button class="product-customizer-modal__close-btn"  @click.native.prevent="hide" size="15"/>
+        <h3 class="product-customizer-modal__title">Looking Good </h3>
+        <p class="product-customizer-modal__content">Nice Design! You are the first person to create this combination. We'd love to feature your item in a photoshoot!</p>
+        <img class="product-customizer-modal__img" :src="productImages[0].thumbnail">
+        <button class="product-customizer-modal__add">COUNT ME IN</button>
+        <button class="product-customizer-modal__close" @click="hide">NO THANKS</button>
       </div>
     </div> 
   </div>
@@ -15,8 +16,12 @@
 
 <script>
 import { mapState } from 'vuex';
+import CloseButton from './CloseButton.vue'
 
  export default {
+  components: {
+    CloseButton,
+  },
    data() {
      return {
       showModal: false
