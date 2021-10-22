03208a85f69c5cd391e2ed7f016da808a4a417c4
Matthew Peters <matt@ambaum.com>
2021-10-22 15:21:24 -0500

    Capitalization

--- widgets/src/components/ProductCustomizer.vue
+++ widgets/src/components/ProductCustomizer.vue
@@ -54,7 +54,7 @@
         </template>
         <template v-else-if="!isClearance">
           Quick Custom, Made in the USA<br>
-          Custom made:
+          Custom Made:
         </template>
         <template v-else>
           Custom made in the USA<br>
