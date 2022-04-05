3e3b0735ef36b007428e534aba35b1090f258c81
Matthew Peters <matt@ambaum.com>
2022-04-05 18:07:30 -0500

    Fix formatting

--- widgets/src/filters/money.js
+++ widgets/src/filters/money.js
@@ -1,3 +1,8 @@
-const money = price => (price ? `$${parseInt(price, 10).toLocaleString()}` : '');
+const options = {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 2
+};
+
+const money = price => (price ? `$${parseInt(price, 10).toLocaleString(undefined, options)}` : '');
 
 export default money;
